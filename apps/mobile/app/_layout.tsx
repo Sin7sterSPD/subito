80fa8e20628cd1d3bd7b9c583b92eaacd2c5a2b2
ShyamGuna77 <gshyamprasad2001@gmail.com>
2026-06-05 19:34:21 +0530

    wip

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -3,17 +3,28 @@ import "../global.css"
 
 import { HeroUINativeProvider } from "heroui-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { queryClient } from "@/src/lib/query-client"
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <HeroUINativeProvider>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        />
-      </HeroUINativeProvider>
-    </GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <HeroUINativeProvider>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="(auth)" options={{ animation: "fade" }} />
+            <Stack.Screen name="(tabs)" options={{ animation: "fade" }} />
+            <Stack.Screen
+              name="(screens)"
+              options={{ animation: "slide_from_right" }}
+            />
+          </Stack>
+        </HeroUINativeProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   )
 }
