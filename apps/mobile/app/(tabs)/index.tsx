4ff04581ded4d5256f3aed7db6dde2cfa441ef07
ShyamGuna77 <gshyamprasad2001@gmail.com>
2026-06-07 11:10:26 +0530

    feat(auth):added prebuilds expo

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native"
+import React from "react"
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text className="text-red-08 items-center justify-center text-lg font-bold">
+        Tab Index
+      </Text>
+      
     </View>
   )
 }
